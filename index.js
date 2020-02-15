1aaf8654126b6b5fcc8cd720e61bf261bdb4e249
Thibaud Maurel <maurel.thibaud@free.fr>
2020-02-15 15:17:18 +0100

    banner bio + project

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+const scrolls = document.querySelectorAll('.scroll-down');
+scrolls.forEach((scroll) => {
+  scroll.addEventListener('click', (event) => {
+    window.scrollBy({
+      top: window.innerHeight, // could be negative value
+      left: 0,
+      behavior: 'smooth'
+    });
+  })
+})
+
+const mySiema = new Siema({
+  duration: 1450,
+  loop: true,
+  easing: 'ease-out',
+});
+
+// listen for keydown event
+setInterval(() => mySiema.next(), 3000)
+
+const mySiema2 = new Siema({
+  selector: '.siema2',
+  duration: 1450,
+  loop: true,
+  easing: 'ease-out',
+});
+
+// listen for keydown event
+setInterval(() => mySiema2.next(), 12000)
